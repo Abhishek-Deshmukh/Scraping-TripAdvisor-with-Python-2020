f37fbf24a328607e633a413c24bc5dcf00e8a7e5
abhishek-deshmukh <deshmukh.abhishek369@gmail.com>
2021-06-14 22:22:53 +0530

    added in a scrapper for hotels

---
+++ hotel_scraper.py
@@ -0,0 +1,49 @@
+import sys
+import csv
+from selenium import webdriver
+import time
+import math
+
+# default path to file to store data
+path_to_file = "./reviews.csv"
+input_file_path = "./urls.txt"
+
+inputFile = open(input_file_path, 'r', encoding="utf-8")
+csvReader = csv.reader(inputFile)
+
+# Open the file to save the review
+csvFile = open(path_to_file, 'a', encoding="utf-8")
+csvWriter = csv.writer(csvFile)
+
+# Import the webdriver
+driver = webdriver.Firefox()
+
+for url in csvReader:
+    driver.get(url[0])
+    hotel_name = driver.find_element_by_xpath(".//h1[@class='_1mTlpMC3']").text
+    # total_number = int(driver.find_element_by_xpath(".//span[@class='_33O9dg0j']").text[:-8])
+    # num_page = math.ceil(total_number/5)
+    # change the value inside the range to save more or less reviews
+    while 1==1:
+        # expand the review
+        time.sleep(2)
+
+        container = driver.find_elements_by_xpath(".//div[@class='_2wrUUKlw _3hFEdNs8']")
+
+
+        for j in range(len(container)):
+
+            container[j].find_element_by_xpath("//a[@class='ocfR3SKN']").click()
+            title = container[j].find_element_by_xpath(".//div[@class='glasR4aX']").text
+            date = container[j].find_element_by_xpath(".//span[@class='_34Xs-BQm']").get_attribute("title")
+            rating = container[j].find_element_by_xpath(".//span[contains(@class, 'ui_bubble_rating bubble_')]").get_attribute("class").split("_")[3]
+            review = container[j].find_element_by_xpath(".//q[@class='IRsGHoPm']").text.replace("\n", " ")
+            csvWriter.writerow([hotel_name, date, rating, title, review])
+
+        try:
+            # change the page
+            driver.find_element_by_xpath('.//a[@class="ui_button nav next primary "]').click()
+        except:
+            break
+
+driver.close()
